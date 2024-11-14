ac5ad5a548098987053827860dc9e60985e1d001
Akayy <techakayy@gmail.com>
2024-11-14 12:41:45 +1100

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import { useHead } from 'unhead'
+
+  definePage({
+    name: 'home',
+    // alias: ['home'],
+    meta: {
+      title: 'Home',
+      navOrder: 1,
+    },
+  })
+
+  useHead({
+    title: 'Home',
+  })
+</script>
+<template>
+  <div>
+    <TheWelcome />
+  </div>
+</template>
+<style scoped></style>
---
+++ src/site.ts
@@ -0,0 +1,14 @@
+// since `.js, .ts` files are not included by default,
+// the following comment tells UnoCSS to force scan this file (to pick the logo icon).
+// @unocss-include
+
+export default {
+  title: 'Vue Designer',
+  description: 'Vue Designer CSS - Quick start template',
+  author: 'Pinegrow',
+  url: 'https://pg-vue-css.netlify.app',
+  nav: [
+    { text: 'Home', link: '/' },
+    { text: 'About', link: '/about' },
+  ],
+}
